ab0927dc27f557fefa338cd395f23aad69f199d3
Claudio Melendres <claudiomelendres@gmail.com>
2020-02-02 12:19:33 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DirectivaComponent } from './components/directiva/directiva.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+
+import {ClienteService} from './components/clientes/cliente.service';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full' },
+  {path: 'directiva', component: DirectivaComponent },
+  {path: 'clientes', component: ClientesComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [ClienteService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
